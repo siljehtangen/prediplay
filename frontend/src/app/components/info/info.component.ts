3f386f90f5779fa43d0b52caedd40405be66a448
Silje Hanø Tangen <siljehtangen@outlook.com>
2026-05-05 18:18:04 +0200

    feat: add translation to info component

--- frontend/src/app/components/info/info.component.ts
+++ frontend/src/app/components/info/info.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { TranslateModule } from '@ngx-translate/core';
+import { SafeHtmlPipe } from '../../pipes/safe-html.pipe';
 
 @Component({
   selector: 'app-info',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatIconModule],
+  imports: [CommonModule, MatCardModule, MatIconModule, TranslateModule, SafeHtmlPipe],
   templateUrl: './info.component.html',
   styleUrl: './info.component.scss'
 })
